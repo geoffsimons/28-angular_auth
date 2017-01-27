4ba9848bd0baa9948f231d7cd1052e948e14ca93
Geoff Simons <geoff.simons@gmail.com>
2017-01-26 21:44:07 -0800

    Changed redirects to point at /landing

--- app/config/router-config.js
+++ app/config/router-config.js
@@ -3,8 +3,8 @@
 module.exports = ['$stateProvider', '$urlRouterProvider', routerConfig];
 
 function routerConfig($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.when('', '/join#signup');
-  $urlRouterProvider.when('/', '/join#signup');
+  $urlRouterProvider.when('', '/landing');
+  $urlRouterProvider.when('/', '/landing');
   $urlRouterProvider.when('/signup', '/join#signup');
   $urlRouterProvider.when('/signin', '/join#signin');
 
